01cd7f5f7b00975b9f54c3b7e6f26490f87bd9d4
namprik <adthasit.coding@gmail.com>
2018-01-28 10:33:00 +0700

    edit word

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,11 +1,16 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+import { 
+    ADD_PLACE, 
+    DELETE_PLACE, 
+    SELECT_PLACE, 
+    DESELECT_PLACE 
+} from '../actions/actionTypes';
 
 const initialState = {
      places: [],
      selectedPlace: null
 };
 
-const reducer = (State = initialState, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type){
         case ADD_PLACE:
             return {
